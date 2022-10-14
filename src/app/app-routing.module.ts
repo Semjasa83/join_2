3f24571f3d0e9a0806c4a7383a262588a2ce923f
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-10-14 17:33:38 +0200

    summary in work, todo task section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  { path: '',   redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
