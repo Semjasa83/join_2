521d6b77297c1da3f44e71aed4336c301dd66259
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-10-17 00:03:55 +0200

    add task hard finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,10 @@ import { AddtaskComponent } from './addtask/addtask.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 
@@ -40,6 +42,9 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
