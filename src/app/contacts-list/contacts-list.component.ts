4ab7a2946b8d3d5b9d6ce1b4e194aa9346672ace
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-11-02 16:49:23 +0100

    id received from contact

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection } from '@firebase/firestore';
+import { Observable } from 'rxjs';
+import { Contact } from 'src/models/contact.class';
 
 @Component({
   selector: 'app-contacts-list',
@@ -7,7 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsListComponent implements OnInit {
 
-  constructor() { }
+  contact = new Contact();
+  allContacts$: Observable<any>;
+  allContacts: any = [];
+
+  constructor(private firestore: Firestore) {
+    
+    const coll = collection(firestore, 'contacts');
+    this.allContacts$ = collectionData(coll, {idField: "contactId"});
+
+    this.allContacts$.subscribe((changes: any) => {
+      console.log('received changes;', changes);
+      this.allContacts = changes;
+      console.log('allContacts: ', this.allContacts );
+    })
+  }
+
+
+
 
   ngOnInit(): void {
   }
