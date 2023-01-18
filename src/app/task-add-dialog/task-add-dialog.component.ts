c6ee75e0cca865fd6aa9a72c0645d79f3e7fcaf6
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2023-01-18 17:58:57 +0100

    add form validator to task
    prepare the validators for add task

--- src/app/task-add-dialog/task-add-dialog.component.ts
+++ src/app/task-add-dialog/task-add-dialog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { ClientService } from '../client.service';
 import { Task} from 'src/models/task.class';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-task-add-dialog',
@@ -12,9 +14,23 @@ export class TaskAddDialogComponent implements OnInit {
 
   task: Task = new Task();
 
-  constructor(private firestore: Firestore, public client: ClientService) { }
+  constructor(private firestore: Firestore, public client: ClientService, private formbuilder: FormBuilder) { }
+
+  taskForm: FormGroup = this.formbuilder.group({
+    title: ['', [Validators.required]],
+    describtion: ['', [Validators.required]],
+    date: ['', [Validators.required]],
+  });
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.task.title = this.taskForm.controls['title'].value;
+    this.task.description = this.taskForm.controls['description'].value;
+    this.task.date = this.taskForm.controls['date'].value;
+    console.log(this.task.title);
+    //this.saveContact();
   }
 
 }
