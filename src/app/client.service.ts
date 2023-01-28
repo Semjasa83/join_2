393ffd11f60bf3b44e21e71e424fe98e0c5147ca
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2023-01-28 11:02:15 +0100

    prepared client Service for Tasks
    Added and prepared CRUD for Tasks in Service

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -15,6 +15,7 @@ import { Firestore, collectionData, docData } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contact } from 'src/models/contact.class';
+import { Task } from 'src/models/task.class';
 
 @Injectable({
   providedIn: 'root'
@@ -27,32 +28,65 @@ export class ClientService {
   private contactsCollection: CollectionReference<DocumentData>;
   private sortedContacts: Query<DocumentData>;
 
+  private tasksCollection: CollectionReference<DocumentData>;
+  private sortedTasks: Query<DocumentData>;
+
   constructor(public readonly firestore: Firestore) {
 
     this.contactsCollection = collection(firestore, 'contacts');
     this.sortedContacts = query(this.contactsCollection, orderBy('firstName', 'asc'));
+
+    this.tasksCollection = collection(firestore, 'tasks');
+    this.sortedTasks = query(this.tasksCollection);
   }
 
   getAllContacts() {
     return collectionData(this.sortedContacts, {idField: 'id',}) as Observable<any>;
   }
 
+  getAllTasks() {
+    return collectionData(this.sortedTasks, {idField: 'id',}) as Observable<any>;
+  }
+
+
   getContact(id: string) {
     const contactsDocumentReference = doc(this.firestore, `contacts/${id}`);
     return docData(contactsDocumentReference, { idField: 'id' });
   }
 
+  getTask(id: string){
+    const tasksDocumentReference = doc(this.firestore, `tasks/${id}`);
+    return docData(tasksDocumentReference, { idField: 'id' });
+  }
+
+
   createContact(contact: Contact) {
     return addDoc(this.contactsCollection, contact);
   }
+
+  createTask(task: Task) {
+    return addDoc(this.tasksCollection, task);
+  }
+
 
   updateContact(contact: Contact) {
     const contactsDocumentReference = doc(this.firestore, `contacts/${contact.id}`);
     return updateDoc(contactsDocumentReference, { ...contact });
   }
 
+  updateTask(task: Task) {
+    const tasksDocumentReference = doc(this.firestore, `tasks/${task.id}`);
+    return updateDoc(tasksDocumentReference, { ...task });
+  }
+
+
   deleteContact(id: string) {
     const contactsDocumentReference = doc(this.firestore, `contacts/${id}`);
     return deleteDoc(contactsDocumentReference);
+  }
+
+  deleteTask(id: string) {
+    const tasksDocumentReference = doc(this.firestore, `tasks/${id}`);
+    return deleteDoc(tasksDocumentReference);
   }
 }
